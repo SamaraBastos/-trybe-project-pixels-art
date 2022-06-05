62e469a228fe333c03ae26975fb8e8ceeb96aa52
Samara Bastos <samarasampaio30@gmail.com>
2022-06-05 18:17:58 -0300

    correçoes

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
-
 const black = document.getElementById('black');
 const brown = document.getElementById('brown');
 const yellow = document.getElementById('yellow');
-const pink = document.getElementById('pink')
-const pixelBoard = document.querySelector('#pixel-board')
+const pink = document.getElementById('pink');
+const pixelBoard = document.querySelector('#pixel-board');
 
 function clicar(evento) {
-    const selecionar = document.querySelector('.selected')
-    selecionar.classList.remove('selected');
-    evento.target.classList.add('selected')
+  const selecionar = document.querySelector('.selected');
+  selecionar.classList.remove('selected');
+  evento.target.classList.add('selected');
 }
 
-black.addEventListener('click', clicar )
-yellow.addEventListener('click', clicar)
-brown.addEventListener('click', clicar )
-pink.addEventListener('click',clicar)
+black.addEventListener('click', clicar);
+yellow.addEventListener('click', clicar);
+brown.addEventListener('click', clicar);
+pink.addEventListener('click', clicar);
 
-function pixels(evento) {
-    const changeColor = document.querySelector('.selected');
-    evento.target.style.backgroundColor = changeColor.id
+function pixels(event) {
+  const changeColor = document.querySelector('.selected');
+  event.target.style.backgroundColor = changeColor.id;
 }
-pixelBoard.addEventListener('click', pixels)
+pixelBoard.addEventListener('click', pixels);
 
 const clear1 = document.getElementById('clear-board');
-const clear2 = document.getElementsByClassName('pixel')
+const clear2 = document.getElementsByClassName('pixel');
 
-function clik (){
-    for( i = 0; i < clear2.length; i += 1)
-    clear2[i].style.backgroundColor = 'white';
+function clik() {
+  for (let i = 0; i < clear2.length; i += 1) 
+  clear2[i].style.backgroundColor = 'white';
 }
-clear1.addEventListener('click', clik)
+
+clear1.addEventListener('click', clik);
