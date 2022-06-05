2009a9c7339f11abb7b546db03edb21fc3719201
Samara Bastos <samarasampaio30@gmail.com>
2022-06-05 17:48:53 -0300

    Requisito 8 e 9

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 
-function recebeClik (evento){
-    let pixelsClicaveis = document.querySelector('.selected')
-    pixelsClicaveis.classList.remove('selected');
-    evento.target.classList.add('selected');
-}       
-let colorPalette = document.querySelector('#color-palette');
-colorPalette.addEventListener('click', recebeClik);
+const black = document.getElementById('black');
+const brown = document.getElementById('brown');
+const yellow = document.getElementById('yellow');
+const pink = document.getElementById('pink')
+const pixelBoard = document.querySelector('#pixel-board')
+
+function clicar(evento) {
+    const selecionar = document.querySelector('.selected')
+    selecionar.classList.remove('selected');
+    evento.target.classList.add('selected')
+}
+
+black.addEventListener('click', clicar )
+yellow.addEventListener('click', clicar)
+brown.addEventListener('click', clicar )
+pink.addEventListener('click',clicar)
+
+function pixels(evento) {
+    const changeColor = document.querySelector('.selected');
+    evento.target.style.backgroundColor = changeColor.id
+}
+pixelBoard.addEventListener('click', pixels)
+
+const clear1 = document.getElementById('clear-board');
+const clear2 = document.getElementsByClassName('pixel')
+
+function clik (){
+    for( i = 0; i < clear2.length; i += 1)
+    clear2[i].style.backgroundColor = 'white';
+}
+clear1.addEventListener('click', clik)
